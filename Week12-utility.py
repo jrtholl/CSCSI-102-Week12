973ef153f16aa354bcfc530a6f5184df7e8a5659
jrtholl <jrtholl@mymail.mines.edu>
2019-11-21 14:40:43 -0700

    edited FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,7 +1,7 @@
 # https://github.com/jrtholl/CSCSI-102-Week12
 # Jared Tholl
 # CSCI 102 - Section E; 11:00am - 11:50am
-# Lab: Week 11 - Part B
+# Lab: Week 12 - Part B
 
 def PrintOutput(x):
     print('OUTPUT ' + x)
